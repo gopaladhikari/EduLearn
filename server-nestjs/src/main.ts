d422b36b8324e4fb464f135d389431d390373c56
gopaladhikari <gopuadks@gmail.com>
2024-12-30 14:50:48 +0545

    updated nestjs global prefix

--- server-nestjs/src/main.ts
+++ server-nestjs/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import type { NestExpressApplication } from '@nestjs/platform-express';
 
@@ -10,8 +10,14 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
-
-  app.setGlobalPrefix('api/v1');
+  app.setGlobalPrefix('api/v1', {
+    exclude: [
+      {
+        path: '/',
+        method: RequestMethod.GET,
+      },
+    ],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

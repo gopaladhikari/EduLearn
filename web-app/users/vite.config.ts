647c7decc8c0f8cb7ffd3dcb65c7f72ea6b10aa9
gopaladhikari <gopuadks@gmail.com>
2025-01-19 12:47:22 +0545

    updated vite congif

--- web-app/users/vite.config.ts
+++ web-app/users/vite.config.ts
@@ -2,7 +2,6 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { flatRoutes } from "remix-flat-routes";
-import netlifyPlugin from "@netlify/vite-plugin-react-router";
 
 declare module "@remix-run/node" {
   interface Future {
@@ -25,6 +24,5 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
-    netlifyPlugin(),
   ],
 });
